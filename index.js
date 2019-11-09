cda9e2116583bcf707958260abfdc9f83067436c
Manuel <manuelsilvadois@gmail.com>
2019-11-09 17:51:50 +0000

    Improved form

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-document.addEventListener('click', function(ev){
+
+// Close the sidebar when user clicks outside it
+document.addEventListener('click', function (ev) {
     const target = ev.target
     const sidebar = document.getElementById('sidebar')
     const in_sidebar = document.getElementById('in-sidebar-floating')
-    
+
     // If the checkbox to open the sidebar was clicked, do nothing
     if (target === in_sidebar) return
 
@@ -10,4 +12,9 @@ document.addEventListener('click', function(ev){
     if (target !== sidebar && !sidebar.contains(target)) {
         in_sidebar.checked = false
     }
+})
+
+// Enable the submit button when form is touched
+document.getElementById('form').addEventListener('input', function (ev) {
+    this.querySelector('button[type="submit"]').removeAttribute('disabled', '')
 })
